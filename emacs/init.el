97533b2541efefbc516a061ba6adece0fcfdcb61
Henning Sudbrock <henning.sudbrock@telekom.de>
2021-08-09 14:45:20 +0200

    Add and configure magit

--- emacs/init.el
+++ emacs/init.el
@@ -76,16 +76,7 @@
 (use-package gitlab-ci-mode)
 (use-package markdown-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(markdown-mode gitlab-ci-mode doom-modeline counsel ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Configure magit
+(use-package magit
+  :bind ("C-M-;" . magit-status)
+  :commands (magit-status magit-get-current-branch))
