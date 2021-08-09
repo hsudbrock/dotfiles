6715c456875b6858070befa252cf69dddde2ef8f
Henning Sudbrock <github@hsudbrock.de>
2021-08-09 13:34:40 +0200

    add markdown, gitlab ci modes to emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -64,15 +64,9 @@
   :ensure t
   :init (doom-modeline-mode 1)
   :custom (doom-modeline-height 15))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline counsel ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+
+;; Add support for markdown, gitlab-ci
+(use-package gitlab-ci-mode)
+(use-package markdown-mode)
+
